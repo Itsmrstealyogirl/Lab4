9f74d91c8cc3323ebccf17f0a2676e6380111311
Satwik Singh <satwik@wirelessprv-10-193-218-204.near.illinois.edu>
2017-09-26 14:43:00 -0500

    Done

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,20 +7,50 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
+/**
+ * Project which is done in LAb.
+ *
+ *
+ *
+ */
 
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+public class ReclamationProject {
+    /**
+     *
+     * @param e String 1.
+     * @param f String 2.
+     * @return r returns longest substring r.
+     */
+    public static String doIt(final String e, final String f) {
+        String a = e, b = f;
+        if (a.length() > b.length()) {
+            String c = a;
+            a = b;
+            b = c; // swap the strings if a > b
+            }
+        String r = "";
         /*
-         * For loop with i
+         * For loop checking if substring is same
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
-}
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = a.length() - i; j > 0; j--) {
+                for (int k = 0; k < b.length() - j; k++) {
+                    if ((a.regionMatches(i, b, k, j) && j > r.length())) {
+                        r = a.substring(i, i + j);
+                        }
+                    }
+                }
+        }
+        return r; // return back the substring obtained above
+        }
+    /**
+     * my main function.
+     * @param args something.
+     */
+    public static void main(final String[] args) {
+        String inp1 = "Wassup";
+        String inp2 = "Sup";
+        doIt(inp1, inp2);
+
+    }
+    }
